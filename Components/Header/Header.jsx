443936fe6ef8bfa21ceaa5e94bd85788b1baf1b5
Pankaj Gupta <guptapankaj6363@gmail.com>
2024-01-26 16:41:22 +0530

    MR-6-navbar-hero-footer

---
+++ Components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Image from "next/image";
+
+
+const Header = () => {
+    return (
+        <div className="relative h-[500px] overflow-hidden">
+            <Image
+                src="/2.jpg"
+                alt="Hero Image"
+                layout="fill"
+                objectFit="cover"
+                className="h-full w-full"
+            />
+            <div className="absolute inset-0 bg-black opacity-40"></div>
+
+            <div className="absolute inset-0 flex items-center justify-center text-white">
+                <div className="text-center">
+                    <h1 className="text-4xl font-bold mb-4">Welcome to Your Recommender App</h1>
+                    <p className="text-lg">Discover personalized recommendations just for you.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header
