b11d6b3ec0e2baffeb6b41e335880366bc6ec55a
Ashish Tiwari <ashish.tiwari@designscript.in>
2024-01-28 22:21:14 +0530

    production-error-4

--- Components/Header/Header.jsx
+++ Components/Header/Header.jsx
@@ -1,14 +1,10 @@
 // components/Header.js
 import React from "react";
-import Image from "next/image";
 
 const Header = () => {
   return (
     <div className="relative max-w-screen-xl mx-auto h-screen">
-      <div className="relative h-full">
-        <Image src="/22.jpg" alt="Hero Image" layout="fill" objectFit="cover" />
-      </div>
-      <div className="absolute inset-0 flex items-center justify-center text-white">
+      <div className="absolute inset-0 flex items-center justify-center text-white bg-blue-800">
         <div className="text-center">
           <h1 className="text-4xl font-bold mb-4">
             Welcome to Your Recommender App
--- app/Dark.jsx
+++ app/Dark.jsx
@@ -1,30 +1,16 @@
 "use client"
-
 import React, { useState } from 'react'
 import Navbar from "@/Components/Navbar/Navbar";
 import Footer from "@/Components/Footer/Footer";
-import Image from "next/image";
 import Header from "@/Components/Header/Header";
 
 const Dark = () => {
-
   const [darkMode, setDarkMode] = useState(false);
-
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-  };
-
   return (
-    <div>
-        <div className="flex flex-col min-h-screen">
+      <div>
       <Navbar setDarkMode={setDarkMode} darkMode={darkMode} />
-      
-      <div className="flex-grow">
-        < Header />
-      </div>
-
+      <Header />
       <Footer darkMode={darkMode} />
-    </div>
     </div>
   )
 }
